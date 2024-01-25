5436797e33bdbc9a1be46bd7079c7b3ad61e8dcf
Brandon Little <blittle@inco.cc>
2024-01-24 22:53:53 -0500

    gamemode: spawn default announcer automatically

--- gamemodes/xtremefootball/gamemode/init.lua
+++ gamemodes/xtremefootball/gamemode/init.lua
@@ -1,3 +1,13 @@
 AddCSLuaFile("cl_init.lua")
 
 include("shared.lua")
+
+function GM:InitPostEntity()
+	if not announcer.IsValid() then
+		local announcer = ents.Create("announcer_s4")
+
+		if announcer:IsValid() then
+			announcer:Spawn()
+		end
+	end
+end
