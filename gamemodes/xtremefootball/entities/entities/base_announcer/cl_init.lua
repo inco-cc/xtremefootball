43a9522de26d4279c314a2bc7d85bcc9876503cc
Brandon Little <blittle@inco.cc>
2024-01-24 22:46:38 -0500

    base_announcer: make announcer omnipresent

--- gamemodes/xtremefootball/entities/entities/base_announcer/cl_init.lua
+++ gamemodes/xtremefootball/entities/entities/base_announcer/cl_init.lua
@@ -4,4 +4,10 @@ function ENT:Initialize()
 	debug.setmetatable(self, debug.getregistry().Announcer)
 
 	announcer.Set(self)
+
+	self:AddEffects(EF_NOINTERP)
+	self:AddEffects(EF_NOSHADOW)
+	self:AddEffects(EF_NODRAW)
+	self:AddEffects(EF_NORECEIVESHADOW)
+	self:AddEffects(EF_NOFLASHLIGHT)
 end
--- gamemodes/xtremefootball/entities/entities/base_announcer/init.lua
+++ gamemodes/xtremefootball/entities/entities/base_announcer/init.lua
@@ -6,4 +6,12 @@ function ENT:Initialize()
 	debug.setmetatable(self, debug.getregistry().Announcer)
 
 	announcer.Set(self)
+
+	self:AddEFlags(EFL_KEEP_ON_RECREATE_ENTITIES)
+	self:AddEFlags(EFL_FORCE_CHECK_TRANSMIT)
+	self:AddEFlags(EFL_NO_THINK_FUNCTION)
+	self:AddEFlags(EFL_NO_GAME_PHYSICS_SIMULATION)
+	self:AddEFlags(EFL_DONTBLOCKLOS)
+	self:AddEFlags(EFL_DONTWALKON)
+	self:AddEFlags(EFL_NO_PHYSCANNON_INTERACTION)
 end
