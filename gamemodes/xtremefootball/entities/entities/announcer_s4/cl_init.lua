07554a3e89e1cb9cdabd0f648674356d611bba6e
Brandon Little <blittle@inco.cc>
2024-01-24 23:16:50 -0500

    announcer_s4: create sound scripts

--- gamemodes/xtremefootball/entities/entities/announcer_s4/cl_init.lua
+++ gamemodes/xtremefootball/entities/entities/announcer_s4/cl_init.lua
@@ -1,1 +1,31 @@
 include("shared.lua")
+
+sound.Add {
+	name = "Announcer.S4.BallReset",
+	sound = "#announcer.s4.ballreset",
+	pitch = { 97, 103 },
+}
+
+sound.Add {
+	name = "Announcer.S4.5Min",
+	sound = "#announcer.s4.5min",
+	pitch = { 97, 103 },
+}
+
+sound.Add {
+	name = "Announcer.S4.3Min",
+	sound = "#announcer.s4.3min",
+	pitch = { 97, 103 },
+}
+
+sound.Add {
+	name = "Announcer.S4.1Min",
+	sound = "#announcer.s4.1min",
+	pitch = { 97, 103 },
+}
+
+sound.Add {
+	name = "Announcer.S4.30Sec",
+	sound = "#announcer.s4.30sec",
+	pitch = { 97, 103 },
+}
